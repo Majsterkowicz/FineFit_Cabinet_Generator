c84d51ee740425d4dee7248bc7e2062157a0c908
Majsterkowicz <lis.lukasz92@gmail.com>
2026-07-22 10:52:10 +0200

    v0.4.0 Added complete project creation workflow

--- FineFitCabinetEngine_v0.1/src/core/application.py
+++ FineFitCabinetEngine_v0.1/src/core/application.py
@@ -1,10 +1,31 @@
 from src.core.menu import Menu
+from src.services.project_manager import ProjectManager
 
 
 class Application:
 
     def __init__(self):
+
         self.menu = Menu()
+
+        self.project_manager = ProjectManager()
+
+        self.current_project = None
+
+    def create_project(self):
+
+        print("=== Nowy projekt ===")
+        print()
+
+        project_name = input("Podaj nazwę projektu: ")
+
+        self.current_project = self.project_manager.create_project(project_name)
+
+        print()
+        print("Projekt został utworzony.")
+        print()
+        print(f"ID projektu: {self.current_project.project_id}")
+        print(f"Nazwa projektu: {self.current_project.project_name}")
 
     def run(self):
 
@@ -15,7 +36,7 @@ class Application:
             print()
 
             if choice == "1":
-                print("Tworzenie nowego projektu...")
+                self.create_project()
 
             elif choice == "2":
                 print("Otwieranie projektu...")
